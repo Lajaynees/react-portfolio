981ddf902ac61dc1031271a71bc75b3ad30feb7f
Lajaynees <lscingram@yahoo.com>
2021-11-10 14:34:15 -0500

    finished Navbar/styled

--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react"
 import logo from "../logo1.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBarsSort} from "@fortawesome/react-fontawesome";
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 
 const Navbar = () => {
@@ -9,7 +9,7 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="robologo"/></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    <FontAwesomeIcon icon= {faBars} style={{ color: "#b65822"}}/>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
